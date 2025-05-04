0cb0f2a28516d3ff545b213a21a6b5c2dcf0acac
fefuchip <tanpinfei@gmail.com>
2025-05-04 21:42:40 +0800

    Edited based on

--- Meetings/meeting_3_(09_04)_task_1.py
+++ Meetings/meeting_3_(09_04)_task_1.py
@@ -1,78 +1,89 @@
+import hashlib
+import random
+
 class client:
-    def __init__(self, name, pkey):
+    def __init__(self, name):
         self.name = name
-        self.pkey = pkey
         
     def register_server(self, serv):
         self.server = serv
+        self.pkey = random.randint(1, 3)
+
+    def register_server_withreferral(self, serv, client):
+        self.server = serv
+        self.pkey = random.randint(1, 3)
+        factor = self.pkey/client.pkey
+        return factor        
     
-    def send_message(self, msg):
-        msg = ord(str(msg))
-        request = msg**self.pkey
-        print (f"{self.name}'s request: {request}")
-        return serv.receive_message(request, self)
+    def blinding_1(self, msg):
+        message = msg
+        msg = hash(message)
+        self.r_1 = random.randint(1, 3)
+        a = (msg**self.pkey)**self.r_1
+        c = self.server.blinding_2(a, self)**(1/self.r_1)
+        return c
+    
+    def register_object(self, msg, object):
+        c = self.blinding_1(msg)      
+        self.server.register_object(c, object)
+
+    def get_object(self, msg):
+        c = self.blinding_1(msg)
+        return self.server.get_object(c)
 
 class server:
-    def __init__(self, finalkey, database):
-        self.finalkey = finalkey
+    def __init__(self):
         self.skeys = {}
-        self.database = database
+        self.database ={}
 
-    def register_client(self, client):
-        skey = self.finalkey / client.pkey
+    def register_client(self, client, referral_client=None):
+        if bool(self.skeys) == False:
+            skey = random.randint(1, 3)
+            client.register_server(self)
+        else:
+            factor = client.register_server_withreferral(self, referral_client)
+            skey = self.skeys[referral_client.name]/factor
         self.skeys[client.name] = skey
+
+    def register_object(self, c, object): 
+        message = int(c**(1/self.r_2))
+        d = hash(message)
+        self.database[d] = object
     
-    def receive_message(self, request, client):
+    def blinding_2(self, a, client):
+        self.r_2 = random.randint(1, 3)
         skey = self.skeys[client.name]
-        finaloutput = request**skey
-        print (f"Final output from {client.name}'s request: {finaloutput}")
-        value = self.database.get(finaloutput)
-        return value
+        b = (a**skey)**self.r_2
+        print(b)
+        return b
     
+    def get_object (self, c):
+        message = int(c**(1/self.r_2))
+        d = hash(message)
+        return self.database[d]
     
-def register(client, server):
-    client.register_server(server)
-    server.register_client(client)
+def hash(message):
+    m = hashlib.md5()
+    m.update(message.encode("utf-8"))
+    return(int(m.hexdigest(), 16))
 
-finalkey = float(input("Choose a final key: "))
+serv = server()
 
-serv = server(finalkey, database = {
-    int(ord(str(2)))**finalkey: "apple",
-    int(ord(str(3)))**finalkey: "orange",
-    int(ord(str(4)))**finalkey: "banana"
-})
+Alice = client("Alice")
+Bob = client("Bob")
 
-valid = False
+serv.register_client(Alice)
+serv.register_client(Bob, Alice)
 
-while not valid:
-    Alice_pkey = int(input("Choose an integer for Alice's private key: "))
-    Alice_skey = finalkey/Alice_pkey
+msg = input("Input an integer from 2 to 4 (Alice's message): ")
+object = input("(Alice) Register an object:")
+Alice.register_object(msg, object)
+Alice_registeredobject = object
 
-    Bob_pkey = int(input("Choose an integer for Bob's private key: "))
-    Bob_skey = finalkey/Bob_pkey
-    
-    if Alice_pkey*Alice_skey == Bob_pkey*Bob_skey:
-        print("Keys are valid")
-        valid = True
-        break
-    else:
-        print("These keys don't work. (For best results, choose integers that the final key is divisible by.)")
+msg = input("Input the same integer as Alice's message for Bob's message:")
+Bob_returnvalue = Bob.get_object(msg)
 
-Alice = client("Alice", Alice_pkey)
-Bob = client("Bob", Bob_pkey)
-
-register(Alice, serv)
-register(Bob, serv)
-
-Alice_msg = int(input("Input an integer from 2 to 4 (Alice's message): "))
-Alice_returnvalue = Alice.send_message(Alice_msg)
-print(f"Item returned: {Alice_returnvalue}")
-
-Bob_msg = int(input("Input the same integer as Alice's message for Bob's message: "))
-Bob_returnvalue = Bob.send_message(Bob_msg)
-print(f"Item returned: {Bob_returnvalue}")
-
-if Alice_returnvalue == Bob_returnvalue:
+if Alice_registeredobject == Bob_returnvalue:
     print("Output is valid")
 else:
     print("Output is invalid")
