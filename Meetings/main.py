5c02f8b780a00954faefe503f3a65442a9ca3868
Brian Tan <briantancs.2020@gmail.com>
2025-05-14 15:13:45 +0800

    implemented fastapi for code

--- Meetings/main.py
+++ Meetings/main.py
@@ -9,7 +9,6 @@ RegisteredDevices = {}
 
 app = FastAPI()
 
-@app.get("/")
 def client_initialize(uid,did,rid):
   if (uid,did,rid) in ClientInitialized:
     return("User " + str(uid) + " has been previsouly initialized.")
@@ -17,7 +16,6 @@ def client_initialize(uid,did,rid):
   else:
     ClientInitialized.append((uid,did,rid))
 
-@app.get("/server_initialize")
 def server_initialize(uid,did,rid):
   if (uid,did,rid) in Registered:
     return("Device " + str(did) + " has been previously registered.")
@@ -27,7 +25,6 @@ def server_initialize(uid,did,rid):
     Registered.append((uid,did,rid))
     return("User " + str(uid) + " has been successfully initialized and device " + str(did) + " has been registered.")
 
-@app.get("/client_start_registration")
 def client_start_registration(uid,did1,did2,rid):
   if (uid,did1,rid) not in ClientInitialized:
     return("User " + str(uid) + " has not been initialized.")
@@ -35,7 +32,6 @@ def client_start_registration(uid,did1,did2,rid):
   else:
     ClientStartRegistration.append((uid,did1,did2,rid))
 
-@app.get("/client_finish_registration")
 def client_finish_registration(uid,did1,did2,rid):
   if (uid,did2,rid) in ClientInitialized:
     return("Device " + str(did2) + " has been previously registered.")
@@ -46,7 +42,6 @@ def client_finish_registration(uid,did1,did2,rid):
   else:
     ClientInitialized.append((uid,did2,rid))
 
-@app.get("/server_accept_registration")
 def server_accept_registration(uid,did1,did2,rid):
   if (uid,did2,rid) in Registered:
     return("Device " + str(did2) + " has been previously registered.")
@@ -66,7 +61,6 @@ def server_accept_registration(uid,did1,did2,rid):
     Registered.append((uid,did2,rid))
     return("User " + str(uid) + " has successfully registered device " + str(did2) + ".")
 
-@app.get("/server_revocation")
 def server_revocation(uid,did,rid):
   if (uid,did,rid) not in Registered:
     return("Device " + str(did) + " has not been registered.")
@@ -75,52 +69,56 @@ def server_revocation(uid,did,rid):
     newDevice=[]
     for i in originalDevice:
       newDevice.append(i)
-    newDevice.remove(did2)
+    newDevice.remove(did)
     RegisteredDevices[(uid,rid)]=newDevice
     Registered.remove((uid,did,rid))
     return("Device " + str(did) + " has been successfully revoked.")
 
-flag1 = True
-flag2 = True
+@app.get("/")
+def fn_selection():
+  flag1 = True
+  flag2 = True
 
-while flag2 == True:
-  command=input("Enter 1 to initialize a new client, 2 to register a new device, 3 to revoke a current device, and 4 to check all registered clients.")
-  if command=="1":
-    uid=input("Username:")
-    did=input("Device:")
-    rid=random.randint(1,10)
-    if uid in ServerKeys.keys():
-      print("User " + str(uid) + " has already been initialized.")
-      flag1 = False
-    else:
-      ServerKeys[uid] = rid
-    if flag1 == True:
-      print(client_initialize(uid,did,rid))
-    if flag1 == True:
-      print(server_initialize(uid,did,rid))
-    flag1 = True
-  elif command=="2":
-    uid=input("Username:")
-    did1=input("Original device:")
-    did2=input("New device:")
-    if uid not in ServerKeys.keys():
-      print("User " + str(uid) + " has not been initialized.")
-      flag1 = False
-    else:
+  while flag2 == True:
+    command=input("Enter 1 to initialize a new client, 2 to register a new device, 3 to revoke a current device, and 4 to check all registered clients.")
+    if command=="1":
+      uid=input("Username:")
+      did=input("Device:")
+      rid=random.randint(1,10)
+      if uid in ServerKeys.keys():
+        print("User " + str(uid) + " has already been initialized.")
+        flag1 = False
+      else:
+        ServerKeys[uid] = rid
+      if flag1 == True:
+        print(client_initialize(uid,did,rid))
+      if flag1 == True:
+        print(server_initialize(uid,did,rid))
+      flag1 = True
+    elif command=="2":
+      uid=input("Username:")
+      did1=input("Original device:")
+      did2=input("New device:")
+      if uid not in ServerKeys.keys():
+        print("User " + str(uid) + " has not been initialized.")
+        flag1 = False
+      else:
+        rid = ServerKeys[uid]
+      if flag1 == True:
+        print(client_start_registration(uid,did1,did2,rid))
+      if flag1 == True:
+        print(client_finish_registration(uid,did1,did2,rid))
+      if flag1 == True:
+        print(server_accept_registration(uid,did1,did2,rid))
+      flag1 = True
+    elif command=="3":
+      uid=input("Username:")
+      did=input("Device to revoke:")
       rid = ServerKeys[uid]
-    if flag1 == True:
-      print(client_start_registration(uid,did1,did2,rid))
-    if flag1 == True:
-      print(client_finish_registration(uid,did1,did2,rid))
-    if flag1 == True:
-      print(server_accept_registration(uid,did1,did2,rid))
-    flag1 = True
-  elif command=="3":
-    uid=input("Username:")
-    did=input("Device to revoke:")
-    rid = ServerKeys[uid]
-    print(server_revocation(uid,did,rid))
-  elif command=="4":
-    print(RegisteredDevices)
-  else:
-    print("Invalid command, please try again.")
+      print(server_revocation(uid,did,rid))
+    elif command=="4":
+      print(RegisteredDevices)
+    else:
+      print("Invalid command, please try again.")
+
+fn_selection()
