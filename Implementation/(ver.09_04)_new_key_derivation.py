cc6f8b63f54f4a1ea497ee0fb119e21f9c6420cb
Brian Tan <briantancs.2020@gmail.com>
2025-09-14 17:24:44 +0800

    based on 18/09 meeting

--- Implementation/(ver.09_04)_new_key_derivation.py
+++ Implementation/(ver.09_04)_new_key_derivation.py
@@ -3,16 +3,18 @@ def keyDev(self):
   for i in range(100): #key is the product of 100 primes (with possible repetition)
     pick = pick.append(random.choice(primeList))
   requirement = False
-  while requirement = False:
+  while requirement == False:
     bitstring = [random.randint(0, 1) for n in range(100)]
     if bitstring.count(1)>=50 and bitstring.count(1)<=70:
       requirement = True
   base = 1
-  for i in range(100):
-    base *= pick[i] if bitstring[i] == 1
   unused = 1
   for i in range(100):
-    unused *= pick[i] if bitstring[i] == 0
+    if bitstring[i] == 1:
+      base *= pick[i]
+    else:
+      unused *= pick[i]
+
   return (base,unused)
 
 def registerDevice(self,name,server):
