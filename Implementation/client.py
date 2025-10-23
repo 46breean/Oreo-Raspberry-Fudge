60cdec930c670e73f9b75f776d69a55390ceb4f1
fefuchip <tanpinfei@gmail.com>
2025-10-23 15:22:37 +0800

    caught value errors for init_reg() and fn_selection() inputs

--- Implementation/client.py
+++ Implementation/client.py
@@ -92,130 +92,138 @@ def inbound_socket(UID, DID, keyproduct):
                 conn.sendall(data_to_send.encode())
 
 def init_reg():
-    print("\nSign Up:")
-    print("1. Initialise user")
-    print("2. Register new device")
-    choice = int(input("Select function: "))
-
-    if choice == 1:
-        name = input("Enter device name: ")
-        DK, unused, keyproduct = keyDev()
-        init = requests.post(f"{SERVER}/init", params={"name": name, "unused": unused}).json()
-        UID, DID = init["UID"], init["DID"]
-        print("Initialised:", init)
-
-        # start listener in background
-        listener_thread = threading.Thread(target=inbound_socket, args=(UID, DID, keyproduct), daemon=True)
-        listener_thread.start()
-        time.sleep(0.5)
-
-        return UID, DID, DK
-
-    elif choice == 2:
-        uid = input("Enter your UID: ")
-        referral_did = input("Enter the DID of your referral device: ")
+    
+    while True:
 
         try:
-            loc = requests.get(
-                f"{SERVER}/device_location",
-                params={"uid": uid, "did": referral_did}
-            )
-            loc.raise_for_status()
-            referral_info = loc.json()
-            referral_ip = referral_info["ip"]
-            referral_port = referral_info["port"]
-        except requests.exceptions.HTTPError as e:
-            print("Could not find referral device:", e.response.json()["detail"])
-            sys.exit(1)
 
-        # connect to referral device
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            print(f"Connecting to referral device at {referral_ip}:{referral_port}...")
-            s.connect((referral_ip, referral_port))
-            s.sendall(b"Registration Request")
-            newdev_msg = s.recv(4096).decode()
-            if newdev_msg == b"REJECTED":
-                print("Registration Request Rejected")
-                sys.exit()
-            else:
-                data = json.loads(newdev_msg)
-                did, dk, unused, keyproduct = (
-                    data["DID"],
-                    data["DK"],
-                    data["unused"],
-                    data["keyproduct"]
-                )
-                did, dk, unused, keyproduct = int(did), int(dk), int(unused), list(keyproduct)
+            print("\nSign Up:")
+            print("1. Initialise user")
+            print("2. Register new device")
+            choice = int(input("Select function: "))
 
-        # start listener
-        listener_thread = threading.Thread(target=inbound_socket, args=(uid, did, keyproduct), daemon=True)
-        listener_thread.start()
-        time.sleep(0.5)
+            if choice == 1:
+                name = input("Enter device name: ")
+                DK, unused, keyproduct = keyDev()
+                init = requests.post(f"{SERVER}/init", params={"name": name, "unused": unused}).json()
+                UID, DID = init["UID"], init["DID"]
+                print("Initialised:", init)
 
-        return uid, did, dk
+                return UID, DID, DK
+
+            elif choice == 2:
+                uid = input("Enter your UID: ")
+                referral_did = input("Enter the DID of your referral device: ")
+
+                try:
+                    loc = requests.get(
+                        f"{SERVER}/device_location",
+                        params={"uid": uid, "did": referral_did}
+                    )
+                    loc.raise_for_status()
+                    referral_info = loc.json()
+                    referral_ip = referral_info["ip"]
+                    referral_port = referral_info["port"]
+                except requests.exceptions.HTTPError as e:
+                    print("Could not find referral device:", e.response.json()["detail"])
+                    sys.exit(1)
+
+                # connect to referral device
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    print(f"Connecting to referral device at {referral_ip}:{referral_port}...")
+                    s.connect((referral_ip, referral_port))
+                    s.sendall(b"Registration Request")
+                    newdev_msg = s.recv(4096).decode()
+                    if newdev_msg == b"REJECTED":
+                        print("Registration Request Rejected")
+                        sys.exit()
+                    else:
+                        data = json.loads(newdev_msg)
+                        did, dk, unused, keyproduct = (
+                            data["DID"],
+                            data["DK"],
+                            data["unused"],
+                            data["keyproduct"]
+                        )
+                        did, dk, unused, keyproduct = int(did), int(dk), int(unused), list(keyproduct)
+
+                return uid, did, dk
+            
+            #start listener
+            listener_thread = threading.Thread(target=inbound_socket, args=(uid, did, keyproduct), daemon=True)
+            listener_thread.start()
+            time.sleep(0.5) 
+            
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue  
 
 def fn_selection(UID, DID, DK):
 
     while True:
-        print("\nDevice Menu:")
-        print("1. Revoke device")
-        print("2. Evaluate")
-        print("3. Exit")
-        choice = int(input("Select function: "))
+        try:
+            print("\nDevice Menu:")
+            print("1. Revoke device")
+            print("2. Evaluate")
+            print("3. Exit")
+            choice = int(input("Select function: "))
 
-        if choice == 1:
-            try:
-                revoke_list = requests.get(
-                    f"{SERVER}/revoke_list",
-                    params = {"uid": UID, "did": DID}
-                )
-                revoke_list.raise_for_status()
-            except requests.exceptions.HTTPError as e:
-                print ("Current device not found")
-                sys.exit(1)
-            
-            print(f"DIDs of registered, not yet revoked devices: {revoke_list.json()}")
+            if choice == 1:
+                try:
+                    revoke_list = requests.get(
+                        f"{SERVER}/revoke_list",
+                        params = {"uid": UID, "did": DID}
+                    )
+                    revoke_list.raise_for_status()
+                except requests.exceptions.HTTPError as e:
+                    print ("Current device not found")
+                    sys.exit(1)
+                
+                print(f"DIDs of registered, not yet revoked devices: {revoke_list.json()}")
 
-            revoke_did = int(input("Select DID to revoke:"))
+                revoke_did = int(input("Select DID to revoke:"))
 
-            revoke = requests.post(
-                f"{SERVER}/revoke",
-                json={"uid": UID, "did": DID, "revoke_did": revoke_did}
-            ).json()
-            print(revoke)
-        elif choice == 2:
-            index = int(input("Enter a number to evaluate: "))
-            hashed_index = hash_int(index) % p
-            r1 = random_coprime(p - 1)
+                revoke = requests.post(
+                    f"{SERVER}/revoke",
+                    json={"uid": UID, "did": DID, "revoke_did": revoke_did}
+                ).json()
+                print(revoke)
+            elif choice == 2:
+                index = int(input("Enter a number to evaluate: "))
+                hashed_index = hash_int(index) % p
+                r1 = random_coprime(p - 1)
 
-            blinded = pow(hashed_index, DK * r1, p)
+                blinded = pow(hashed_index, DK * r1, p)
 
-            try:
-                resp1 = requests.post(
-                    f"{SERVER}/eval/step1",
-                    json={"uid": UID, "did": DID, "blinded": blinded}
-                )
-                resp1.raise_for_status()
-                blinded2 = resp1.json()["blinded2"]
-            except requests.exceptions.HTTPError as e:
-                print("Step 1 failed:", e.response.json()["detail"])
-                input("Press Enter to continue...")
-                return
-            except KeyError:
-                print("Unexpected response from server:", resp1.json())
-                input("Press Enter to continue...")
-                return
+                try:
+                    resp1 = requests.post(
+                        f"{SERVER}/eval/step1",
+                        json={"uid": UID, "did": DID, "blinded": blinded}
+                    )
+                    resp1.raise_for_status()
+                    blinded2 = resp1.json()["blinded2"]
+                except requests.exceptions.HTTPError as e:
+                    print("Step 1 failed:", e.response.json()["detail"])
+                    input("Press Enter to continue...")
+                    return
+                except KeyError:
+                    print("Unexpected response from server:", resp1.json())
+                    input("Press Enter to continue...")
+                    return
 
-            r1_inv = pow(r1, -1, p - 1)
-            unblinded1 = pow(blinded2, r1_inv, p)
+                r1_inv = pow(r1, -1, p - 1)
+                unblinded1 = pow(blinded2, r1_inv, p)
 
-            resp2 = requests.post(f"{SERVER}/eval/step2", json={"uid": UID, "did": DID, "unblinded1": unblinded1}).json()
-            print("Encrypted Index:", resp2["final"])
-        elif choice == 3:
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice.")
+                resp2 = requests.post(f"{SERVER}/eval/step2", json={"uid": UID, "did": DID, "unblinded1": unblinded1}).json()
+                print("Encrypted Index:", resp2["final"])
+            elif choice == 3:
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid choice.")
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue
 
 p = requests.get(f"{SERVER}/config").json()["p"]
 primeList = primes.upto(104729)
