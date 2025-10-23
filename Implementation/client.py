f7f62806ad261cf896759a63fc985b50cb3beaef
fefuchip <tanpinfei@gmail.com>
2025-10-23 16:51:37 +0800

    fixed some errors

--- Implementation/client.py
+++ Implementation/client.py
@@ -69,7 +69,7 @@ def inbound_socket(UID, DID, keyproduct):
                 tmp.close()
 
                 subprocess.Popen([
-                    "start", "cmd", "/k",
+                    "start", "cmd", "/c",
                     sys.executable, "registration.py",
                     str(UID), str(DID),
                     str(addr),
@@ -104,12 +104,19 @@ def init_reg():
 
             if choice == 1:
                 name = input("Enter device name: ")
-                DK, unused, keyproduct = keyDev()
+                dk, unused, keyproduct = keyDev()
                 init = requests.post(f"{SERVER}/init", params={"name": name, "unused": unused}).json()
-                UID, DID = init["UID"], init["DID"]
+                uid, did = init["UID"], init["DID"]
                 print("Initialised:", init)
 
-                return UID, DID, DK
+                print (f"UID: {uid}")
+
+                #start listener
+                listener_thread = threading.Thread(target=inbound_socket, args=(uid, did, keyproduct), daemon=True)
+                listener_thread.start()
+                time.sleep(0.5) 
+
+                return uid, did, dk
 
             elif choice == 2:
                 uid = input("Enter your UID: ")
@@ -146,13 +153,13 @@ def init_reg():
                             data["keyproduct"]
                         )
                         did, dk, unused, keyproduct = int(did), int(dk), int(unused), list(keyproduct)
+                    
+                #start listener
+                listener_thread = threading.Thread(target=inbound_socket, args=(uid, did, keyproduct), daemon=True)
+                listener_thread.start()
+                time.sleep(0.5) 
 
                 return uid, did, dk
-            
-            #start listener
-            listener_thread = threading.Thread(target=inbound_socket, args=(uid, did, keyproduct), daemon=True)
-            listener_thread.start()
-            time.sleep(0.5) 
             
         except ValueError:
             print("Invalid input. Please try again.")
@@ -161,69 +168,65 @@ def init_reg():
 def fn_selection(UID, DID, DK):
 
     while True:
-        try:
-            print("\nDevice Menu:")
-            print("1. Revoke device")
-            print("2. Evaluate")
-            print("3. Exit")
-            choice = int(input("Select function: "))
+        print("\nDevice Menu:")
+        print("1. Revoke device")
+        print("2. Evaluate")
+        print("3. Exit")
+        choice = int(input("Select function: "))
 
-            if choice == 1:
-                try:
-                    revoke_list = requests.get(
-                        f"{SERVER}/revoke_list",
-                        params = {"uid": UID, "did": DID}
-                    )
-                    revoke_list.raise_for_status()
-                except requests.exceptions.HTTPError as e:
-                    print ("Current device not found")
-                    sys.exit(1)
-                
-                print(f"DIDs of registered, not yet revoked devices: {revoke_list.json()}")
+        if choice == 1:
+            try:
+                revoke_list = requests.get(
+                    f"{SERVER}/revoke_list",
+                    params = {"uid": UID, "did": DID}
+                )
+                revoke_list.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                print ("Current device not found")
+                sys.exit(1)
+            
+            print(f"DIDs of registered, not yet revoked devices: {revoke_list.json()}")
 
-                revoke_did = int(input("Select DID to revoke:"))
+            revoke_did = int(input("Select DID to revoke:"))
 
-                revoke = requests.post(
-                    f"{SERVER}/revoke",
-                    json={"uid": UID, "did": DID, "revoke_did": revoke_did}
-                ).json()
-                print(revoke)
-            elif choice == 2:
-                index = int(input("Enter a number to evaluate: "))
-                hashed_index = hash_int(index) % p
-                r1 = random_coprime(p - 1)
+            revoke = requests.post(
+                f"{SERVER}/revoke",
+                json={"uid": UID, "did": DID, "revoke_did": revoke_did}
+            ).json()
+            print(revoke)
+        elif choice == 2:
+            index = int(input("Enter a number to evaluate: "))
+            hashed_index = hash_int(index) % p
+            r1 = random_coprime(p - 1)
 
-                blinded = pow(hashed_index, DK * r1, p)
+            blinded = pow(hashed_index, DK * r1, p)
 
-                try:
-                    resp1 = requests.post(
-                        f"{SERVER}/eval/step1",
-                        json={"uid": UID, "did": DID, "blinded": blinded}
-                    )
-                    resp1.raise_for_status()
-                    blinded2 = resp1.json()["blinded2"]
-                except requests.exceptions.HTTPError as e:
-                    print("Step 1 failed:", e.response.json()["detail"])
-                    input("Press Enter to continue...")
-                    return
-                except KeyError:
-                    print("Unexpected response from server:", resp1.json())
-                    input("Press Enter to continue...")
-                    return
+            try:
+                resp1 = requests.post(
+                    f"{SERVER}/eval/step1",
+                    json={"uid": UID, "did": DID, "blinded": blinded}
+                )
+                resp1.raise_for_status()
+                blinded2 = resp1.json()["blinded2"]
+            except requests.exceptions.HTTPError as e:
+                print("Step 1 failed:", e.response.json()["detail"])
+                input("Press Enter to continue...")
+                return
+            except KeyError:
+                print("Unexpected response from server:", resp1.json())
+                input("Press Enter to continue...")
+                return
 
-                r1_inv = pow(r1, -1, p - 1)
-                unblinded1 = pow(blinded2, r1_inv, p)
+            r1_inv = pow(r1, -1, p - 1)
+            unblinded1 = pow(blinded2, r1_inv, p)
 
-                resp2 = requests.post(f"{SERVER}/eval/step2", json={"uid": UID, "did": DID, "unblinded1": unblinded1}).json()
-                print("Encrypted Index:", resp2["final"])
-            elif choice == 3:
-                print("Goodbye!")
-                break
-            else:
+            resp2 = requests.post(f"{SERVER}/eval/step2", json={"uid": UID, "did": DID, "unblinded1": unblinded1}).json()
+            print("Encrypted Index:", resp2["final"])
+        elif choice == 3:
+            print("Goodbye!")
+            break
+        else:
                 print("Invalid choice.")
-        except ValueError:
-            print("Invalid input. Please try again.")
-            continue
 
 p = requests.get(f"{SERVER}/config").json()["p"]
 primeList = primes.upto(104729)
