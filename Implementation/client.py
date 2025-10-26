88d901ac3de38d15ce0c2af8f7bed2e4a671ef48
fefuchip <tanpinfei@gmail.com>
2025-10-26 23:27:46 +0800

    added server port

--- Implementation/client.py
+++ Implementation/client.py
@@ -1,7 +1,7 @@
 import requests, random, math, hashlib, socket, sys, threading, time, subprocess, tempfile, os, json
 from primePy import primes
 
-SERVER = "http://192.168.68.65"
+SERVER = "http://192.168.68.65:8000"
 
 def hash_int(x: int) -> int:
     m = hashlib.sha256()
